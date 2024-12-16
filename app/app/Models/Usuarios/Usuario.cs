1598f2331acfa2430305acb530baf59ee8200705
L3ORIOS <sr.leobardo@gmail.com>
2024-12-16 13:18:04 +0100

    set point

---
+++ app/app/Models/Usuarios/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.Models.Usuarios
+{
+    public class Usuario
+    {
+        private string _email;
+        private string _password;
+
+        public string Email { get => _email; set => _email = value; }
+        public string Password { get => _password; set => _password = value; }
+    }
+}
