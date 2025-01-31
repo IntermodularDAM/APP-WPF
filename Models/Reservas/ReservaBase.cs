77c9b09f0557ba048cc9f91340ec3db3cfc0a4ee
N1ghtFr0st <71523252+N1ghtFr0st@users.noreply.github.com>
2025-01-31 19:49:14 +0100

    Add files via upload

---
+++ Models/Reservas/ReservaBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservas_Final.Models.Reservas
+{
+    public class ReservaBase
+    {
+
+        public string _id { get; set; }
+        public string id_usu { get; set; }
+        public string id_hab { get; set; }
+        public string fecha_check_in { get; set; }
+        public string fecha_check_out { get; set; }
+        public string estado_reserva { get; set; }
+    }
+}
